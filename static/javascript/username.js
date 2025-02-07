53f15a70067d0982f4f0eb9fa72f8113c5cadf05
Diamondshatter2 <alistairfolster@gmail.com>
2025-02-07 01:49:30 -0500

    added more text to lobby, slight css selector refactoring

--- static/javascript/username.js
+++ static/javascript/username.js
@@ -1,11 +1,8 @@
 const username_socket = io.connect();
 
 document.addEventListener('DOMContentLoaded', () => {
-    const button = document.querySelector('#username button');
-    const form = document.getElementById('username_form');
-    
-    button.addEventListener('click', function() {
+    document.querySelector('#username button').addEventListener('click', function() {
         this.parentElement.style.display = 'none';
-        form.style.display = 'flex';
+        document.querySelector('#username form').style.display = 'flex';
     });
 });
