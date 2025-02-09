115f06072c1a42bd6f50729554d135a36f4f2850
Diamondshatter2 <alistairfolster@gmail.com>
2025-02-08 21:25:55 -0500

    deleted irrelevant file

--- username.py
+++
@@ -1,7 +0,0 @@
-def propagate_username_change(games, player_id, new_username):
-    for game in games.values():
-        for i in [1, 2]:
-            if game.players[i] == player_id:
-                game.usernames[i] = new_username
-                return
-
