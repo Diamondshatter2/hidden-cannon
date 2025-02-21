1e8db47d263eef3be5c7a11723e8cb8bba475988
Diamondshatter2 <alistairfolster@gmail.com>
2025-02-20 23:45:02 -0500

    frontend working

--- static/javascript/game.js
+++ static/javascript/game.js
@@ -1,32 +1,16 @@
-let columns, indicator;
+document.addEventListener('DOMContentLoaded', () => {
+    var board = Chessboard('board', {
+        draggable: true,
+        dropOffBoard: 'snapback',
+        position: 'start',
+        pieceTheme: '/static/images/{piece}.png'
+    });
+})
 
-const drop_sounds = [1, 2, 3, 4, 5].map(number => new Audio(`/static/audio/disc-drop-${number}.mp3`));
 const end_sound = new Audio('static/audio/game-end.mp3');
 
 const game_id = new URLSearchParams(window.location.search).get('game_id');
 const socket = io.connect({ query: { game_id } });
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    columns = Array.from(document.querySelectorAll('.column'));
-    indicator = document.getElementById('indicator');
-
-    columns.forEach(column => column.addEventListener('click', request_move));
-});
-
-function request_move() {
-    socket.emit('request move', columns.indexOf(this));
-}
-
-socket.on('make move', move_data => {
-    const space = columns[move_data["column"]].children[move_data["row"]];
-    const player = move_data["player"];
-
-    drop_sounds[Math.floor(Math.random() * drop_sounds.length)].play(); // Plays random drop sound
-    space.style.backgroundColor = colors[player];
-    indicator.firstElementChild.style.backgroundColor = colors[player ^ 1]; // var bitwise-XOR 1 toggles var between 0 and 1
-});
-
 
 socket.on('end game', result => {
     end_sound.play();
