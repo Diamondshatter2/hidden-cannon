ed1f0f4a0ad49c75f23b4832c44ff9164aeff9cb
Diamondshatter2 <alistairfolster@gmail.com>
2025-02-27 23:02:54 -0500

    cannon tracking

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import chess
+from numpy import sign
 
 
 class Game:
@@ -31,15 +32,35 @@ class Game:
             move_type = "capture" if self.board.is_capture(move) else "normal"
             self.board.push(move) 
             self.fen = self.board.fen()
+
+            for type in ['rook', 'bishop']:
+                if move_data["from"] in self.cannons[type]: # second time checking for this, obviously needs refactoring
+                    self.cannons[type][self.whose_turn] = move_data["to"]
+
             self.whose_turn = 1 - self.whose_turn # Toggle between 0 and 1
             return move_type, self.fen
         
     
     def make_rook_cannon_move(self, move_data):
-        if not (move_data["from"][0] == move_data["to"][0] or move_data["from"][1] == move_data["to"][1]):
+        if move_data["from"][0] == move_data["to"][0] and move_data["from"][1] != move_data["to"][1]:
+            abs_step = 8
+        elif move_data["from"][1] == move_data["to"][1] and move_data["from"][0] != move_data["to"][0]:
+            abs_step = 1
+        else:
             return
+
         
-        print("rook cannon move attemped")
+        from_index = chess.parse_square(move_data["from"])
+        to_index = chess.parse_square(move_data["to"])
+        step = abs_step * sign(to_index - from_index)
+        path_to_target = list(range(from_index + step, to_index, step))
+        pieces_between = [self.board.piece_at(square) for square in path_to_target]
+        print(path_to_target)
+        print(pieces_between, "\n\n\n")
+
+        
+
+
         
 
     def make_bishop_cannon_move(self, move_data):
