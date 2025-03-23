f59b56a447bc9f788da57ec514654d268f0e1499
Diamondshatter2 <alistairfolster@gmail.com>
2025-03-23 17:07:04 -0400

    minor changes

--- server.py
+++ server.py
@@ -2,6 +2,12 @@ from flask import Flask, session, render_template, request; from flask_socketio 
 from uuid import uuid4 as generate_id; from secrets import token_hex; from random import choice
 from game import Game_state, ROOK, BISHOP
 
+
+app = Flask(__name__); socket = SocketIO(app, async_mode="eventlet")
+app.secret_key = token_hex(32) 
+games = {}
+
+
 class Game:
     def __init__(self, name):
         self.name = name
@@ -10,10 +16,6 @@ class Game:
         self.messages = []
         self.state = Game_state()
         
-app = Flask(__name__)
-app.secret_key = token_hex(32) 
-socketio = SocketIO(app, async_mode="eventlet")
-games = {}
 
 @app.before_request
 def assign_player_id_and_username():
@@ -27,8 +29,8 @@ def assign_player_id_and_username():
 
 
 @app.errorhandler(404) 
-def serve_404_page(e):
-    return render_template("404.html", missing="page", username=session["username"]), 404
+def serve_404_page(error):
+    return render_template("404.html", missing="page", username=session["username"]), error
 
 
 @app.route("/", methods=["GET", "POST"]) 
@@ -57,14 +59,14 @@ def serve_game():
     return render_template("game.html", game=game, player=player, username=session["username"], rook=ROOK, bishop=BISHOP)
 
 
-@socketio.on("connect")
+@socket.on("connect")
 def add_connection_to_room():
     game_id = request.args.get("game_id")
     if game_id:
         join_room(game_id)
 
 
-@socketio.on("new game")
+@socket.on("new game")
 def create_game(color):
     game_id = str(generate_id())
     game_name = f"Game by {session["username"]}"
@@ -76,11 +78,11 @@ def create_game(color):
     games[game_id].players[color] = session["player_id"]
     games[game_id].usernames[color] = session["username"]
 
-    socketio.emit("redirect to game", game_id, room=request.sid)
-    socketio.emit("add game to list", { "id": game_id, "name": game_name })
+    socket.emit("redirect to game", game_id, room=request.sid)
+    socket.emit("add game to list", { "id": game_id, "name": game_name })
 
 
-@socketio.on("connect to game")
+@socket.on("connect to game")
 def connect_to_game():
     game_id = request.args.get("game_id")
     game = games[game_id]
@@ -91,16 +93,16 @@ def connect_to_game():
             game.usernames[player] = session["username"]
 
             # Seat number is opposite of player number because seats are displayed as "White: <username>, "Black: <username>"
-            socketio.emit("grant seat", { "number": 1 - player, "user": session["username"] }, room=game_id) 
+            socket.emit("grant seat", { "number": 1 - player, "user": session["username"] }, room=game_id) 
             if player == 0:
-                socketio.emit("flip board", room=request.sid)
-                
-            socketio.emit("offer cannon selection", ROOK, room=request.sid)
+                socket.emit("flip board", room=request.sid)
+
+            socket.emit("offer cannon selection", ROOK, room=request.sid)
 
             break
 
 
-@socketio.on("select cannon")
+@socket.on("select cannon")
 def initialize_cannon(selection):
     game = games[request.args["game_id"]]
     piece = selection["piece"]
@@ -114,17 +116,17 @@ def initialize_cannon(selection):
     positions = {"Q": "a", "K": "h"} if piece == ROOK else {"Q": "c", "K": "f"}            
     game.state.cannons[piece][player] = positions[selection["side"]] + ("1" if player == 1 else "8")
 
-    socketio.emit("highlight cannon", game.state.cannons[piece][player], room=request.sid)
+    socket.emit("highlight cannon", game.state.cannons[piece][player], room=request.sid)
 
     if piece == ROOK:
-        socketio.emit("offer cannon selection", BISHOP, room=request.sid)
+        socket.emit("offer cannon selection", BISHOP, room=request.sid)
 
     if None not in game.state.cannons[BISHOP]:
         game.state.is_active = True
-        socketio.emit("begin game", room=request.args["game_id"])
+        socket.emit("begin game", room=request.args["game_id"])
 
 
-@socketio.on("request move")
+@socket.on("request move")
 def handle_move_request(move): # switch move and move_data variable names
     game_id = request.args.get("game_id")
     if game_id not in games:
@@ -132,27 +134,27 @@ def handle_move_request(move): # switch move and move_data variable names
 
     game = games[game_id]
     if game.players[game.state.board.turn] != session["player_id"]:
-        socketio.emit("update board state", game.state.board.fen(), room=request.sid)
+        socket.emit("update board state", game.state.board.fen(), room=request.sid)
         return
     
     move_data = game.state.handle_move_request(move["source"], move["target"], move["promotion"])
     if not move_data:
-        socketio.emit("update board state", game.state.board.fen(), room=request.sid)
+        socket.emit("update board state", game.state.board.fen(), room=request.sid)
         return
     
-    socketio.emit("play move sound", move_data["is capture"], room=game_id)
-    socketio.emit("update board state", move_data["fen"], room=game_id)
+    socket.emit("play move sound", move_data["is capture"], room=game_id)
+    socket.emit("update board state", move_data["fen"], room=game_id)
     if move_data["reveal cannon"]:
-        socketio.emit("highlight cannon", move_data["reveal cannon"], room=game_id)
+        socket.emit("highlight cannon", move_data["reveal cannon"], room=game_id)
 
     if game.state.outcome:
-        socketio.emit("end game", game.state.outcome, room=game_id)
+        socket.emit("end game", game.state.outcome, room=game_id)
 
     if move["promotion"]:
-        socketio.emit("alert", "Promotion piece choice is still in development, but here's a queen!", room=request.sid)
+        socket.emit("alert", "Promotion piece choice is still in development, but here's a queen!", room=request.sid)
 
 
-@socketio.on("offer draw")
+@socket.on("offer draw")
 def offer_draw():
     game_id = request.args.get("game_id")
     game = games[game_id]
@@ -162,7 +164,7 @@ def offer_draw():
     # NEED FUNCTIONALITY HERE
 
 
-@socketio.on("resign")
+@socket.on("resign")
 def handle_resignation_request():
     game_id = request.args.get("game_id")
     game = games[game_id]
@@ -173,11 +175,11 @@ def handle_resignation_request():
         if session["player_id"] == game.players[i]:
             game.state.outcome = f"{game.state.colors[i - 1]} wins by resignation"
             game.state.is_active = False
-            socketio.emit("end game", game.state.outcome, room=game_id)
+            socket.emit("end game", game.state.outcome, room=game_id)
             break
 
 
-@socketio.on("submit message")
+@socket.on("submit message")
 def send_message(message_content):
     if not message_content or set(message_content) == { " " }:
         return
@@ -185,8 +187,8 @@ def send_message(message_content):
     game_id = request.args.get("game_id")
     message = { "sender": session["username"], "content": message_content }
     games[game_id].messages.append(message)
-    socketio.emit("update chat", message, room=game_id)
+    socket.emit("update chat", message, room=game_id)
     
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socket.run(app, host="0.0.0.0", port=5000, debug=True)
